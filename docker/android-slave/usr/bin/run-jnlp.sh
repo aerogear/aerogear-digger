295cebd0ae532a5120e5176f39b7823d9277df46
Luigi Zuccarelli <lzuccare@redhat.com>
2017-05-31 17:36:38 +0100

    Updated Dockerfile and script to fix minor bugs

--- docker/android-slave/usr/bin/run-jnlp.sh
+++ docker/android-slave/usr/bin/run-jnlp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -gt 2 ]
+check_params=$(echo "$1 $2" | grep -o "cordova\|node\|npm\|java")
+
+if [[ "$check_params" == "cordova" ]] ||  [[ "$check_params" == "java" ]] || [[ "$check_params" == "node" ]] || [[ "$check_params" == "npm" ]] 
 then
+  exec "$@"
+else
   # execute the original entry point with parameters
   /usr/local/bin/run-jnlp-client "$@"
 fi
-
-exec "$@"

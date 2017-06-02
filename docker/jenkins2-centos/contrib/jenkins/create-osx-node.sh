b09dd7e6c7907beb6f0f9628991b5771fe307d21
Oleg Matskiv <omatskiv@redhat.com>
2017-06-02 14:20:51 +0200

    Fix folder structure during OSX node creation

--- docker/jenkins2-centos/contrib/jenkins/create-osx-node.sh
+++ docker/jenkins2-centos/contrib/jenkins/create-osx-node.sh
@@ -3,7 +3,7 @@
 if [ ! -z "${OSX_SLAVE_HOST}" ]; then
     echo "Copying Jenkins OSX node configuration to ${JENKINS_HOME}/nodes ..."
     mkdir -p ${image_config_dir}
-    cp -r ${image_config_dir}/nodes ${JENKINS_HOME}/nodes
+    cp -r ${image_config_dir}/nodes/* ${JENKINS_HOME}/nodes
     rm -rf ${image_config_dir}/nodes
 
     envsubst < "${JENKINS_HOME}/nodes/digger-ios/config.xml.tpl" > "${JENKINS_HOME}/nodes/digger-ios/config.xml"

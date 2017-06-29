02390d04ebe90c67c8433dc9aa11dfaedfe9a1d9
Laura Fitzgerald <lfitzger@redhat.com>
2017-06-29 14:42:43 +0100

    update-android-jenkins-file

--- jenkinsfiles/android_jenkinsfile.groovy
+++ jenkinsfiles/android_jenkinsfile.groovy
@@ -1,13 +1,46 @@
 /**
 * Android Jenkinsfile
 */
-node('android') {
-    stage 'Checkout'
+node("android"){
+  stage("Checkout"){
     checkout scm
+  }
 
-    stage 'Build'
-    sh "./gradlew clean assembleDebug"
+  stage ("Prepare"){
+    writeFile file: 'app/src/main/assets/fhconfig.properties', text: params.FH_CONFIG_CONTENT
+  }
 
-    stage 'Archive'
-    archiveArtifacts artifacts: 'app/build/outputs/apk/*.apk', excludes: 'app/build/outputs/apk/*-unaligned.apk'
+  stage("Build"){
+    sh 'chmod +x ./gradlew'
+    if (params.BUILD_CONFIG == 'release') {
+      sh './gradlew clean assembleRelease' // builds app/build/outputs/apk/app-release.apk file
+    } else {
+      sh './gradlew clean assembleDebug' // builds app/build/outputs/apk/app-debug.apk
+    }
+  }
+
+  stage("Sign"){
+    if (params.BUILD_CONFIG == 'release') {
+        signAndroidApks (
+            keyStoreId: "${params.BUILD_CREDENTIAL_ID}",
+            keyAlias: "${params.BUILD_CREDENTIAL_ALIAS}",
+            apksToSign: "**/*-unsigned.apk",
+            // uncomment the following line to output the signed APK to a separate directory as described above
+            // signedApkMapping: [ $class: UnsignedApkBuilderDirMapping ],
+            // uncomment the following line to output the signed APK as a sibling of the unsigned APK, as described above, or just omit signedApkMapping
+            // you can override these within the script if necessary
+            // androidHome: '/usr/local/Cellar/android-sdk'
+        )
+    } else {
+      println('Debug Build - Using default developer signing key')
+    }
+  }
+
+ stage("Archive"){
+    if (params.BUILD_CONFIG == 'release') {
+        archiveArtifacts artifacts: 'app/build/outputs/apk/app-release.apk', excludes: 'app/build/outputs/apk/*-unaligned.apk'
+    } else {
+        archiveArtifacts artifacts: 'app/build/outputs/apk/app-debug.apk', excludes: 'app/build/outputs/apk/*-unaligned.apk'
+    }
+  }
 }

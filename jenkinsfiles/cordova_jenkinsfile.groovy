aeb21930373efc4a0501c1698f7dbaf0ba3cfb13
Laura Fitzgerald <lfitzger@redhat.com>
2017-07-11 15:23:46 +0100

    adding android build to cordova

--- jenkinsfiles/cordova_jenkinsfile.groovy
+++ jenkinsfiles/cordova_jenkinsfile.groovy
@@ -1,14 +1,63 @@
 /**
 * Cordova Jenkinsfile
 */
-node('android') {
-   stage 'Checkout'
-	 checkout scm
 
-	 stage 'Prepare'
-	 cordova platform add android
-	 cordova prepare
+def platform = params?.PLATFORM?.trim()
+def buildType = params?.BUILD_CONFIG?.trim()
 
-	 stage 'Build'
-	 cordova build android
+node(platform) {
+    stage("Checkout") {
+        checkout scm
+    }
+
+    stage("Prepare") {
+        sh "cordova platform add $platform"
+        sh "cordova prepare $platform"
+    }
+
+    stage("Build") {
+        if (platform == 'android') {
+            if (buildType == 'debug') {
+               sh 'cordova build $platform --buildConfig build.json'
+            } else {
+               sh 'cordova build $platform --release'
+            }
+        }
+    }
+
+    stage("Sign") {
+        if (platform == 'android') {
+            if (params.BUILD_CONFIG == 'release') {
+                signAndroidApks (
+                    keyStoreId: "${params.BUILD_CREDENTIAL_ID}",
+                    keyAlias: "${params.BUILD_CREDENTIAL_ALIAS}",
+                    apksToSign: "platforms/android/**/*-unsigned.apk",
+                    // uncomment the following line to output the signed APK to a separate directory as described above
+                    // signedApkMapping: [ $class: UnsignedApkBuilderDirMapping ],
+                    // uncomment the following line to output the signed APK as a sibling of the unsigned APK, as described above, or just omit signedApkMapping
+                    // you can override these within the script if necessary
+                    // androidHome: '/usr/local/Cellar/android-sdk'
+                )
+            } else {
+                println('Debug Build - Using default developer signing key')
+            }
+        }    
+    }
+
+    stage("Archive") {
+        if (platform == 'android') {
+            if (buildType == 'release') {
+                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-release.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
+            } else {
+                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-debug.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
+            }
+        }
+        if (platform == 'ios') {
+            if (buildType == 'release') {
+                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/release.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
+            } else {
+                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/debug.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
+            }
+        }
+    }
 }

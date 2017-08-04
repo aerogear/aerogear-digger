ca10aab94e404131977889edf2da22a1ea9c583f
Laura Fitzgerald <lfitzger@redhat.com>
2017-08-04 12:13:45 +0100

    update Jenkinsfiles

--- jenkinsfiles/cordova_jenkinsfile.groovy
+++ jenkinsfiles/cordova_jenkinsfile.groovy
@@ -2,8 +2,21 @@
 * Cordova Jenkinsfile
 */
 
-def platform = params?.PLATFORM?.trim()
-def buildType = params?.BUILD_CONFIG?.trim()
+//     in RHMAP's case, the following parameters are sent by RHMAP to Jenkins job.
+//     this means, the Jenkins job must be a parametrized build with those parameters.
+def platform = params?.PLATFORM?.trim()                      // e.g. "ios" or "android"
+BUILD_CONFIG = params?.BUILD_CONFIG?.trim()                 // e.g. "Debug" or "Release"
+CODE_SIGN_PROFILE_ID = params?.BUILD_CREDENTIAL_ID?.trim()   // e.g. "redhat-dist-dp"
+
+//     To hardcode values uncomment the lines below
+//CODE_SIGN_PROFILE_ID = "redhat-dist-dp"
+//BUILD_CONFIG = "debug"
+
+// sample values commented below are for https://github.com/feedhenry-templates/helloworld-app
+/* ------------- use these to hardcode values in Jenkinsfile ---------------- */
+PROJECT_NAME = "Helloworld"
+CLEAN = true                          // Do a clean build and sign
+
 
 node(platform) {
     stage("Checkout") {
@@ -11,23 +24,22 @@ node(platform) {
     }
 
     stage("Prepare") {
+        sh "cordova platform rm $platform"
         sh "cordova platform add $platform"
         sh "cordova prepare $platform"
     }
 
     stage("Build") {
-        if (platform == 'android') {
-            if (buildType == 'debug') {
-               sh 'cordova build $platform --buildConfig build.json'
+            if (BUILD_CONFIG == 'debug') {
+               sh 'cordova build $platform --debug'
             } else {
                sh 'cordova build $platform --release'
             }
-        }
     }
 
     stage("Sign") {
         if (platform == 'android') {
-            if (params.BUILD_CONFIG == 'release') {
+            if (BUILD_CONFIG == 'release') {
                 signAndroidApks (
                     keyStoreId: "${params.BUILD_CREDENTIAL_ID}",
                     keyAlias: "${params.BUILD_CREDENTIAL_ALIAS}",
@@ -41,23 +53,23 @@ node(platform) {
             } else {
                 println('Debug Build - Using default developer signing key')
             }
-        }    
+        }
+        if (platform == 'ios') {
+            codeSign(
+                profileId: "${CODE_SIGN_PROFILE_ID}",
+                clean: CLEAN,
+                verify: true,
+                appPath: "platforms/ios/build/emulator/${PROJECT_NAME}.app"
+            )
+        }
     }
 
     stage("Archive") {
         if (platform == 'android') {
-            if (buildType == 'release') {
-                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-release.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            } else {
-                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-debug.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            }
+            archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-${BUILD_CONFIG}.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
         }
         if (platform == 'ios') {
-            if (buildType == 'release') {
-                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/release.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            } else {
-                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/debug.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            }
+            archiveArtifacts artifacts: "platforms/ios/build/emulator/${PROJECT_NAME}.ipa"
         }
     }
 }

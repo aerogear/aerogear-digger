31cd54bd4020f79f2d69a1d5bf61facf8877a10f
Leonardo Rossetti <lrossett@redhat.com>
2017-09-07 13:10:00 -0300

    updated cordova jenkins file

--- jenkinsfiles/cordova_jenkinsfile.groovy
+++ jenkinsfiles/cordova_jenkinsfile.groovy
@@ -16,7 +16,18 @@ CODE_SIGN_PROFILE_ID = params?.BUILD_CREDENTIAL_ID?.trim()   // e.g. "redhat-dis
 /* ------------- use these to hardcode values in Jenkinsfile ---------------- */
 PROJECT_NAME = "Helloworld"
 CLEAN = true                          // Do a clean build and sign
+INFO_PLIST = "Helloworld/Helloworld-Info.plist"
+VERSION = "1.0.0"
+SHORT_VERSION = "1.0"
+BUNDLE_ID = "org.feedhenry.dart.helloworld.cordova"
+OUTPUT_FILE_NAME="${PROJECT_NAME}-${BUILD_CONFIG}.ipa".replace(" ", "").toLowerCase()
+SDK = "iphoneos"
 
+if (BUILD_CONFIG.toLowerCase() == "debug") {
+    OSX_BUILD_CONFIG = "Debug"
+} else if(BUILD_CONFIG.toLowerCase() == "release" || BUILD_CONFIG.toLowerCase() == "distribution") {
+    OSX_BUILD_CONFIG = "Release"
+}
 
 node(platform) {
     stage("Checkout") {
@@ -24,16 +35,36 @@ node(platform) {
     }
 
     stage("Prepare") {
+        sh 'npm install --production'
         sh "cordova platform rm ${platform}"
         sh "cordova platform add ${platform}"
         sh "cordova prepare ${platform}"
+        sh 'rm -rf node_modules'
     }
 
     stage("Build") {
-        if (BUILD_CONFIG == 'debug') {
-            sh "cordova build ${platform} --debug"
+        if (platform == 'android') {
+            if (BUILD_CONFIG == 'debug') {
+                sh "cordova build ${platform} --debug"
+            } else {
+                sh "cordova build ${platform} --release"
+            }
         } else {
-            sh "cordova build ${platform} --release"
+            xcodeBuild(
+                cleanBeforeBuild: CLEAN,
+                src: "./platforms/${platform}",
+                schema: "${PROJECT_NAME}",
+                workspace: "${PROJECT_NAME}",
+                buildDir: "build",
+                sdk: "${SDK}",
+                version: "${VERSION}",
+                shortVersion: "${SHORT_VERSION}",
+                bundleId: "${BUNDLE_ID}",
+                infoPlistPath: "${INFO_PLIST}",
+                xcodeBuildArgs: 'ENABLE_BITCODE=NO OTHER_CFLAGS="-fstack-protector -fstack-protector-all"',
+                autoSign: false,
+                config: "${OSX_BUILD_CONFIG}"
+            )
         }
     }
 
@@ -59,7 +90,8 @@ node(platform) {
                 profileId: "${CODE_SIGN_PROFILE_ID}",
                 clean: CLEAN,
                 verify: true,
-                appPath: "platforms/ios/build/emulator/${PROJECT_NAME}.app"
+                ipaName: OUTPUT_FILE_NAME,
+                appPath: "platforms/ios/build/${OSX_BUILD_CONFIG}-${SDK}/${PROJECT_NAME}.app"
             )
         }
     }
@@ -69,7 +101,7 @@ node(platform) {
             archiveArtifacts artifacts: "platforms/android/build/outputs/apk/android-${BUILD_CONFIG}.apk", excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
         }
         if (platform == 'ios') {
-            archiveArtifacts artifacts: "platforms/ios/build/emulator/${PROJECT_NAME}.ipa"
+            archiveArtifacts artifacts: "platforms/${platform}/build/${OSX_BUILD_CONFIG}-${SDK}/${OUTPUT_FILE_NAME}"
         }
     }
 }

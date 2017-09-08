7a033dff07ab12fdab32b2b24d0c50c4e671c27c
Leonardo Rossetti <lrossett@redhat.com>
2017-09-08 11:29:20 -0300

    latest cordova jenkinsfile

--- jenkinsfiles/cordova_jenkinsfile.groovy
+++ jenkinsfiles/cordova_jenkinsfile.groovy
@@ -2,8 +2,32 @@
 * Cordova Jenkinsfile
 */
 
-def platform = params?.PLATFORM?.trim()
-def buildType = params?.BUILD_CONFIG?.trim()
+//     in RHMAP's case, the following parameters are sent by RHMAP to Jenkins job.
+//     this means, the Jenkins job must be a parametrized build with those parameters.
+def platform = params?.PLATFORM?.trim()                      // e.g. "ios" or "android"
+BUILD_CONFIG = params?.BUILD_CONFIG?.trim()                 // e.g. "Debug" or "Release"
+CODE_SIGN_PROFILE_ID = params?.BUILD_CREDENTIAL_ID?.trim()   // e.g. "redhat-dist-dp"
+
+//     To hardcode values uncomment the lines below
+//CODE_SIGN_PROFILE_ID = "redhat-dist-dp"
+//BUILD_CONFIG = "debug"
+
+// sample values commented below are for https://github.com/feedhenry-templates/helloworld-app
+/* ------------- use these to hardcode values in Jenkinsfile ---------------- */
+PROJECT_NAME = "Helloworld"
+CLEAN = true                          // Do a clean build and sign
+INFO_PLIST = "${PROJECT_NAME}/${PROJECT_NAME}-Info.plist"
+VERSION = "1.0.0"
+SHORT_VERSION = "1.0"
+BUNDLE_ID = "org.feedhenry.dart.helloworld.cordova"
+OUTPUT_FILE_NAME="${PROJECT_NAME}-${BUILD_CONFIG}.ipa".replace(" ", "").toLowerCase()
+SDK = "iphoneos"
+
+if (BUILD_CONFIG.toLowerCase() == "debug") {
+    OSX_BUILD_CONFIG = "Debug"
+} else if(BUILD_CONFIG.toLowerCase() == "release" || BUILD_CONFIG.toLowerCase() == "distribution") {
+    OSX_BUILD_CONFIG = "Release"
+}
 
 node(platform) {
     stage("Checkout") {
@@ -11,23 +35,42 @@ node(platform) {
     }
 
     stage("Prepare") {
-        sh "cordova platform add $platform"
-        sh "cordova prepare $platform"
+        sh 'npm install --production'
+        sh "cordova platform rm ${platform}"
+        sh "cordova platform add ${platform}"
+        sh "cordova prepare ${platform}"
+        sh 'rm -rf node_modules'
     }
 
     stage("Build") {
         if (platform == 'android') {
-            if (buildType == 'debug') {
-               sh 'cordova build $platform --buildConfig build.json'
+            if (BUILD_CONFIG == 'debug') {
+                sh "cordova build ${platform} --debug"
             } else {
-               sh 'cordova build $platform --release'
+                sh "cordova build ${platform} --release"
             }
+        } else {
+            xcodeBuild(
+                cleanBeforeBuild: CLEAN,
+                src: "./platforms/${platform}",
+                schema: "${PROJECT_NAME}",
+                workspace: "${PROJECT_NAME}",
+                buildDir: "build",
+                sdk: "${SDK}",
+                version: "${VERSION}",
+                shortVersion: "${SHORT_VERSION}",
+                bundleId: "${BUNDLE_ID}",
+                infoPlistPath: "${INFO_PLIST}",
+                xcodeBuildArgs: 'ENABLE_BITCODE=NO OTHER_CFLAGS="-fstack-protector -fstack-protector-all"',
+                autoSign: false,
+                config: "${OSX_BUILD_CONFIG}"
+            )
         }
     }
 
     stage("Sign") {
         if (platform == 'android') {
-            if (params.BUILD_CONFIG == 'release') {
+            if (BUILD_CONFIG == 'release') {
                 signAndroidApks (
                     keyStoreId: "${params.BUILD_CREDENTIAL_ID}",
                     keyAlias: "${params.BUILD_CREDENTIAL_ALIAS}",
@@ -41,23 +84,24 @@ node(platform) {
             } else {
                 println('Debug Build - Using default developer signing key')
             }
-        }    
+        }
+        if (platform == 'ios') {
+            codeSign(
+                profileId: "${CODE_SIGN_PROFILE_ID}",
+                clean: CLEAN,
+                verify: true,
+                ipaName: OUTPUT_FILE_NAME,
+                appPath: "platforms/ios/build/${OSX_BUILD_CONFIG}-${SDK}/${PROJECT_NAME}.app"
+            )
+        }
     }
 
     stage("Archive") {
         if (platform == 'android') {
-            if (buildType == 'release') {
-                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-release.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            } else {
-                archiveArtifacts artifacts: 'platforms/android/build/outputs/apk/android-debug.apk', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            }
+            archiveArtifacts artifacts: "platforms/android/build/outputs/apk/android-${BUILD_CONFIG}.apk", excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
         }
         if (platform == 'ios') {
-            if (buildType == 'release') {
-                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/release.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            } else {
-                archiveArtifacts artifacts: 'platforms/ios/Build/Debug-iphoneos/debug.ipa', excludes: 'platforms/android/build/outputs/apk/*-unaligned.apk'
-            }
+            archiveArtifacts artifacts: "platforms/${platform}/build/${OSX_BUILD_CONFIG}-${SDK}/${OUTPUT_FILE_NAME}"
         }
     }
 }
